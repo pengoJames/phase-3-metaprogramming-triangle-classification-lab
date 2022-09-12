ee2fa17b6cdf6e0b7b51584114c2f285e9e419f4
Pe-ngo <jamespengo95@gmail.com>
2022-09-12 08:58:18 +0300

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
   # write code here
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    check_triangle
+  
+    if @a == @b && @b ==@c
+      :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      :isosceles  
+    elsif @a != @b && @b != @c && @a != @c
+      :scalene
+    end
+  end
+
+  def all_positive_sides
+    @a > 0 && @b > 0 && @c > 0
+  end
+
+  def triangle_inequality_checker
+    @a + @b > @c && @a + @c > @b && @c + @b > @a
+
+  end
+
+  def check_triangle
+    if !all_positive_sides  || !triangle_inequality_checker
+      raise TriangleError
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle!"
+    end
+  end
 end
